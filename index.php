6e29339d9b13ba117fbd094965d257952edf2d10
Ville Murtonen <murtonen@gmail.com>
2012-04-07 14:39:57 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<head>
+
+<title></title>
+
+</head>
+<body>
+
+<p><H1> Hei! Tervetuloa valtavan hienoon pankkiohjelmaan! </H1></p>
+<form action="http://www.cs.uta.fi/~vm92179/script.php" method="post">
+Tilinumero: <input type="text" name="tilin"><BR>
+Summa: <input type="text" name="summa"><BR> 
+<input type="submit" value="submit">
+</form>
+
+</body>
+</html>
